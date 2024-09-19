b696e517d811f010aef4265a30954a6d46ac3231
Priya Sidhaiyan <priyasidhaiyan@priyas-mbp.home>
2024-09-19 21:17:57 +0200

    Modified question.js

--- src/question.js
+++ src/question.js
@@ -1,7 +1,16 @@
 class Question {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+constructor (text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+}
+shuffleChoices(){
+  for(let i = this.choices.length-1; i>0; i--)
+  {
+    const j = Math.floor(Math.random() * (this.choices.length));
+    this.choices[i] = this.choices[j];
+  }
+}
 }
